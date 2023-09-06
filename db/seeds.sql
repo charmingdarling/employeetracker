2b5e53fc02733f3acbaaecf924dd241b658d2f6b
Charming Darling <h1hasinai_f06@yahoo.com>
2023-09-06 03:41:55 -0700

    Aesthetic changes to the seeds. I installed extensions that let me read my values in a better format.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,30 +1,29 @@
 INSERT INTO
     neonatal_departments (name)
-VALUES ("Neonatal ICU"), ("Intermediate Care Nursery"), ("Newborn Well Baby"), ("Management");
+VALUES  ("Neonatal ICU"),
+        ("Intermediate Care Nursery"),
+        ("Newborn Well Baby"),
+        ("Management");
 
 INSERT INTO
     role (title, hourly, department_id)
-VALUES ("Clinical Nurse II", 85, 3), ("Clinical Nurse III", 100, 1), ("Clinical Nurse IV", 115, 1), (
-        "Clinical Nurse I, New Grad",
-        4,
-        85
-    )
+VALUES  ("Clinical Nurse II", 85, 3), 
+        ("Clinical Nurse III", 100, 1), 
+        ("Clinical Nurse IV", 115, 1), 
+        ("Clinical Nurse I - New Grad", 4, 85);
 
-INSERT INTO
-    employees (
-        firstName,
-        lastName,
-        role_id,
-        manager_id
-    )
-VALUES ("Clarice", "Kwong", 3), ("Kim", "Nguyen", 1,), ("Ashley", "Gremillion", 4), ("Ysabel", "Anton", 4), ("Bitsy", "Behrendt", 1,), ("Denise", "Woodworth", 1), ("Raquel", "Romero",), ("Kelli", "Lozada",), ("Thione", "Tran",), ("Jessica", "Kim",), (
-        "Sergio",
-        "Vidales-Perez",
-        4,
-        NULL
-    ), ("Grace", "Nelson", 4, NULL), ("Susan", "Wheeler", 4, NULL), (
-        "Florence",
-        "Nightingale",
-        4,
-        NULL
-    );
+INSERT INTO employees (firstName, lastName, role_id, manager_id)
+VALUES ("Florence", "Nightingale", 4, NULL),
+       ("Grace", "Nelson", 4, NULL),
+       ("Sergio", "Vidales-Perez", 4, NULL),
+       ("Susan", "Wheeler", 4, NULL),
+       ("Bitsy", "Behrendt", 1, 1),
+       ("Denise", "Woodworth", 1, 2),
+       ("Kim", "Nguyen", 1, 2),
+       ("Thione", "Tran", 1, 1),
+       ("Raquel", "Romero", 2, 3),
+       ("Clarice","Kwong", 3, 3),
+       ("Kelli","Lozada", 3, 4),
+       ("Ashley", "Gremillion", 4, 2),
+       ("Jessica", "Kim", 4, 4),
+       ("Ysabel", "Anton", 4, 2);
