eab5423b14a7ab48dc3100cea95e67310124a89b
Charming Darling <h1hasinai_f06@yahoo.com>
2023-09-09 21:46:48 -0700

    Adding async/await to printBanner and figlet function so that the Nurse Banner prints well.

--- index.js
+++ index.js
@@ -31,20 +31,32 @@ db.connect((err) => {
 //? });
 
 // Function to start menu.js. File prompts user with questions.
-function printBanner() {
-  // Figlet to print out terminal "Employee Tracker" text
-  figlet(` \n Nurse Tracker `, function (err, data) {
-    if (err) {
-      console.log("Something went wrong... Try something else.");
-      console.dir(err);
-      return;
-    }
-    console.log(data);
+// Need async for printBanner();
+async function printBanner() {
+  console.log("Nurse Tracker should be printed");
+  try {
+    const result = await figletAsync("Nurse Tracker"); //Need await
+    console.log(result);
+  } catch (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+  }
+}
+
+function figletAsync(text) {
+  return new Promise((resolve, reject) => {
+    figlet(text, (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(data);
+    });
   });
 }
 
-function init() {
-  printBanner();
+async function init() {
+  await printBanner(); // Need function init() to be async so that await printBanner works.
   menu();
 }
 
