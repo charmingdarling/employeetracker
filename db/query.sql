7c4d606fea83848567e194c2c5876368af4a9e38
Charming Darling <h1hasinai_f06@yahoo.com>
2023-09-10 01:50:23 -0700

    Wrote a TODO. Need to figure out how to get function viewDepartment() to work. Added Query to JOIN employee full name, title, and manager name. Learned that I can have aliases to represent different representations.

--- db/query.sql
+++ db/query.sql
@@ -1,15 +1,45 @@
-SELECT roles.id, roles.title, neonatal_departments.name AS Neonatal_Department FROM roles 
-LEFT JOIN neonatal_departments 
-ON roles.department_id = neonatal_departments.id
+--This table on line 2 is useless, TBH
+SELECT
+  roles.id,
+  roles.title,
+  neonatal_departments.name AS Neonatal_Department
+FROM
+  roles
+  LEFT JOIN neonatal_departments ON roles.department_id = neonatal_departments.id;
 
-SELECT neonatal_departments.id, neonatal_departments.name, SUM(roles.hourly) AS Shift_Budget FROM employees
-LEFT JOIN roles
-ON employees.role_id = roles.id
-LEFT JOIN neonatal_departments
-ON roles.department_id = neonatal_departments.id
-GROUP BY neonatal_departments.id, neonatal_departments.name;
+-- Budget
+SELECT
+  neonatal_departments.id,
+  neonatal_departments.name,
+  SUM(roles.hourly) AS Shift_Budget
+FROM
+  employees
+  LEFT JOIN roles ON employees.role_id = roles.id
+  LEFT JOIN neonatal_departments ON roles.department_id = neonatal_departments.id
+GROUP BY
+  neonatal_departments.id,
+  neonatal_departments.name;
 
--- Find all employees, bring in the departments that the employee belongs to and brought int he manager ADD
+-- Employee Full Name, Title, Manager Name
+-- I've used e as an alias for the employees table representing employees.
+-- I've used m as an alias for the employees table representing managers.
+SELECT
+  CONCAT (e.firstName, ' ', e.lastName) AS Employee,
+  r.title AS Role_Title,
+  CONCAT (m.firstName, ' ', m.lastName) AS Manager_Name
+FROM
+  employees e
+  LEFT JOIN roles r ON e.role_id = r.id
+  LEFT JOIN employees m ON e.manager_id = m.id
+GROUP BY
+  e.firstName,
+  e.lastName,
+  r.title,
+  Manager_Name;
+
+--
+-- <Break>
+-- Find all employees, bring in the departments that the employee belongs to and brought in the manager ADD
 -- Employee table > join left > role > join left department (role belongs to) > join left employee with manager_id
 -- concat(manager.firstName, " ", manager.LastName) AS manager (to rename for better reading)
 --
