2a3fdcba25dc1417a3d6deb7fc37c44df1522871
Charming Darling <h1hasinai_f06@yahoo.com>
2023-09-10 13:20:29 -0700

    Clearing up notes

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
-// Packages needed for this app
-const mysql = require("mysql2");
+// Packages and Scripts needed for this app
 const figlet = require("figlet");
-
-// Importing Scripts
 const menu = require("./lib/menu");
 
-// Connect to MySQL database, db is in a separate file 'nurses_db'
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root", // MySQL username
-  password: "root", // MySQL password
-  database: "nurses_db", // Connecting to nurse database
-});
-
-// Establish database connection
-db.connect((err) => {
-  if (err) {
-    console.error("Unable to connect to database:", err);
-    return;
-  } else {
-    console.log(`Connected to the nurses_db database.`);
-  }
-});
-
-// Function to start menu.js. File prompts user with questions.
+// Function to start menu.js.
 // Need async for printBanner();
 async function printBanner() {
   console.log("Nurse Tracker should be printed");
   try {
-    const result = await figletAsync("Nurse Tracker"); //Need await
+    const result = await figletAsync("Nurse Tracker"); // Need await
     console.log(result);
   } catch (err) {
     console.log("Something went wrong...");
@@ -48,8 +27,9 @@ function figletAsync(text) {
   });
 }
 
+// Need function init() to be async so that await printBanner works.
 async function init() {
-  await printBanner(); // Need function init() to be async so that await printBanner works.
+  await printBanner();
   menu();
 }
 
