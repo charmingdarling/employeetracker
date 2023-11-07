ecf40cffd93e7a84cbf96c74c2380a4263c2438e
charmingdarling <h1hasinai_f06@yahoo.com>
2023-11-07 01:34:58 -0800

    logging status of connection

--- config/database.js
+++ config/database.js
@@ -1,18 +1,23 @@
 const mysql = require("mysql2");
 
-//? Connect to the database
+// Connect to the database
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "nurses_db",
+});
 
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "nurses_db",
-  },
-  console.log("Established connection to database.")
-);
+// Log connection status
+db.connect((error) => {
+  if (error) {
+    console.log("Unable to establish connection to database.", error);
+    return;
+  }
+  console.log("Connection to database established.");
+});
 
-//? Stop connection to the database
+// Stop connection to the database
 function closeConnection() {
   db.end((error) => {
     if (error) {
